1e4c4f5a540bb1fa33c09cca2ab45afdebf5b805
Gremious Grenald <gremious@protonmail.com>
2020-04-14 19:41:08 +0100

    Initial Commit

---
+++ HarmonyMod/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Xml;
+using UnityEngine;
+using HarmonyLib;
+using HarmonyMod;
+// ReSharper disable UnusedType.Global
+// ReSharper disable UnusedMember.Local
+
+[HarmonyPatch(typeof(FocusCtrl))]
+[HarmonyPatch("Start")]
+class Patch
+{
+    private static void Postfix()
+    {
+        LogHelper.GremyLogger("Test log!! aaaa");
+    }
+}
---
+++ HarmonyMod/LogHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HarmonyMod
+{
+    public static class LogHelper
+    {
+        private static int logInt = 0;
+
+        public static void GremyLogger(String message)
+        {
+            logInt++;
+            Console.WriteLine("Gremy Log " + logInt + ": " + message);
+        }
+    }
+}
